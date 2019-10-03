aa3a513c2c919a8add07fe2b4542f7307819467f
Joe Boylson <joeboylson@gmail.com>
2019-10-03 19:39:39 -0400

    fixing landing page

--- src/components/Pages/LandingPage.tsx
+++ src/components/Pages/LandingPage.tsx
@@ -9,8 +9,7 @@ const LandingPage: React.FC<LandingPageProps> = (Props) => {
     <div>
       <h1>LandingPage</h1>
 
-      {/* use passed continue property to set 'page' in App.tsx */}
-      <div onClick={() => Props.continue(1)} style={{cursor: 'pointer'}}>
+      <div>
         <h2>Welcome to my portfolio.</h2>
         <br/><br/>
         <p>This is a template built off of the <a href="https://create-react-app.dev/">Create React App</a>.</p>
@@ -18,6 +17,10 @@ const LandingPage: React.FC<LandingPageProps> = (Props) => {
         <p>Feel free to fork this repository and use it however you wish!</p>
         <p>#happyhacking</p>
         <br/><br/>
+      </div>
+
+      {/* use passed continue property to set 'page' in App.tsx */}
+      <div onClick={() => Props.continue(1)} style={{cursor: 'pointer'}}>
         <p>Continue to Home</p>
       </div>
     </div>
