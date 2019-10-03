c15342372121a1c960c70b60ede12b391e8181e4
Joe Boylson <joeboylson@gmail.com>
2019-10-03 19:24:49 -0400

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+
+import Nav from '../Nav/Nav';
+import Page from './Page';
+
+// pages
+import LandingPage from '../Pages/LandingPage';
+import Home from '../Pages/Home';
+import Contact from '../Pages/Contact';
+import About from '../Pages/About';
+
+
+const App: React.FC = () => {
+
+
+  const [page, setPage] = React.useState(0)
+
+  // do not display nav if on landing page
+  const navStyle = {
+    opacity: `${page > 0 ? 1 : 0}`,
+    pointerEvents: `${page > 0 ? '' : 'none'}`,
+    transitionDuration: '0.5s'
+  } as React.CSSProperties;
+
+  // hide page if not current page
+  const pageStyle = (pageNumber:number) => {
+    return {
+      transform: `translateX(${ pageNumber === page ? '0' : '-100%' })`,
+      opacity: `${ pageNumber === page ? '1' : '0' }`,
+    } as React.CSSProperties;
+  }  
+
+
+  return (
+    <div id='app'>
+
+      {/* pass in setPage function; pass in navstyle */}
+      <Nav setPage={setPage} style={navStyle}/>
+
+      <main id='main'>
+
+        {/* pass in setPage function */}
+        {/* call pageStyle() to return styling specific to each page */}
+        <Page style={pageStyle(0)}>
+          <LandingPage continue={setPage}/>
+        </Page>
+
+        <Page style={pageStyle(1)}>
+          <Home />
+        </Page>
+
+        <Page style={pageStyle(2)}>
+          <About />
+        </Page>
+
+        <Page style={pageStyle(3)}>
+          <Contact />
+        </Page>
+      </main>
+    
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Pages/LandingPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export interface LandingPageProps {
+  continue: any;
+}
+
+const LandingPage: React.FC<LandingPageProps> = (Props) => {
+  return (
+    <div>
+      <h1>LandingPage</h1>
+
+      {/* use passed continue property to set 'page' in App.tsx */}
+      <div onClick={() => Props.continue(1)} style={{cursor: 'pointer'}}>
+        <h2>Welcome to my portfolio.</h2>
+        <br/><br/>
+        <p>This is a template built off of the <a href="https://create-react-app.dev/">Create React App</a>.</p>
+        <p>It is meant to give you a clean navigation with a few template pages to start out with.</p>
+        <p>Feel free to fork this repository and use it however you wish!</p>
+        <p>#happyhacking</p>
+        <br/><br/>
+        <p>Continue to Home</p>
+      </div>
+    </div>
+  )
+};
+
+export default LandingPage;
